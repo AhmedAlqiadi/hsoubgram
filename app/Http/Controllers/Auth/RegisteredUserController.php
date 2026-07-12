3c6df5df210dfea2e6d4fcb2780736ef67e104e0
AhmedAlqiadi <148564666+AhmedAlqiadi@users.noreply.github.com>
2026-07-12 16:46:18 +0300

    create breeze

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,16 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'username' => ['required', 'string', 'max:30'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        // https://ui-avatars.com/api/?name=John+Doe
         $user = User::create([
             'name' => $request->name,
+            'image'=> 'https://ui-avatars.com/api/?name='. urlencode($request->name),
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
@@ -46,6 +50,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('welcome'));
     }
 }
