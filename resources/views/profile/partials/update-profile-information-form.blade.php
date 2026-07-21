ec18db8374b5926d270bc9c33b038655e564c8b4
AhmedAlqiadi <148564666+AhmedAlqiadi@users.noreply.github.com>
2026-07-21 21:56:21 +0300

    update profile form

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -67,27 +67,14 @@
         </div>
 
 
-      <label class="inline-flex items-center cursor-pointer">
-    <input type="checkbox" class="sr-only peer">
-
-    <div class="relative w-11 h-6 bg-gray-200 rounded-full peer
-        peer-checked:bg-blue-600
-        after:content-['']
-        after:absolute
-        after:top-[2px]
-        after:left-[2px]
-        after:bg-white
-        after:rounded-full
-        after:h-5
-        after:w-5
-        after:transition-all
-        peer-checked:after:translate-x-5">
-    </div>
-
-    <span class="ml-3 text-sm font-medium text-gray-900">
-        Private Account
-    </span>
-</label>
+        <label class="inline-flex items-center cursor-pointer">
+            <input type="checkbox" name="private_account" id="private_account" class="sr-only peer"
+                {{ $user->private_account ? 'checked' : '' }}>
+            <div
+                class="relative w-9 h-5 bg-neutral-quaternary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-brand-soft dark:peer-focus:ring-brand-soft rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-buffer after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-brand">
+            </div>
+            <span class="select-none ms-3 text-sm font-medium text-heading">{{ __('Private Account') }}</span>
+        </label>
 
 
         <div class="flex items-center gap-4">
